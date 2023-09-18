1e1293489d3baa2ce9b08238134b7eb898f12b37
ARIEL DOS SANTOS DIAS <ariel.dias2030@gmail.com>
2023-09-18 09:03:03 -0300

    ajustando linha

--- universe.py
+++ universe.py
@@ -1,5 +1,12 @@
 resposta = 0
+contador = 0
 
 while resposta != 42:
-    resposta = int(input("Qual o numero do universo ? E tudo mais: "))
+    try:
+        contador += b
+        if contador >= 10:
+            break
+        resposta = int(input("Qual o numero do universo ? E tudo mais: "))
+    except:
+        print("Erro")
     
