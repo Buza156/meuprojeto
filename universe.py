c9cc8ceb9a62cb88a69737556315f61c87ed0e74
ARIEL DOS SANTOS DIAS <ariel.dias2030@gmail.com>
2023-09-18 09:29:03 -0300

    Mudando algo

--- universe.py
+++ universe.py
@@ -1,12 +1,9 @@
-resposta = 0
-contador = 0
-
-while resposta != 42:
-    try:
-        contador += b
-        if contador >= 10:
-            break
-        resposta = int(input("Qual o numero do universo ? E tudo mais: "))
-    except:
-        print("Erro")
-    
+try:
+    idade = int(input("Digite a sua idade:"))
+except ValueError as error:
+    print("Erro:",error)
+except Exception as error:
+   print("Aconteceu um erro", error)
+   
+else:
+    print(f"Voce tem {idade} anos")    
